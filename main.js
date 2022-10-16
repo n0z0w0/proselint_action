159ec052880a95e19fbfc0612f84f11b569360da
n0z0w0 <h02dvvd@gmail.com>
2022-10-16 19:01:18 +0900

    use bin/bash

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ const main = async () => {
 		const result = await execute('pwd');
 		console.log('result is as follows\n');
 		console.log(`${result}`);
-		const result2 = await execute('cd ' + workplace);
+		const result2 = await execute('/bin/bash cd ' + workplace);
 		console.log(`${result2}`);
 		const result3 = await execute('ls');
 		console.log(`${result3}`);
