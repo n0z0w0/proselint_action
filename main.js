6d0a0953c2f40a21c606da2d67f18a84462b5303
n0z0w0 <h02dvvd@gmail.com>
2022-10-16 18:44:36 +0900

    rewrite main.js for js

--- main.js
+++ main.js
@@ -3,17 +3,28 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 const writeGood = require('write-good');
 
-let output = '';
-const options = {};
-options.listeners = {
-	stdout: (data) => {
-		output += data.toString();
-	},
-	stderr: (data) => {
-		console.error(data);
-	}
+const execute = async (command) => {
+	let output = '';
+	const options = {};
+	options.listeners = {
+		stdout: (data) => {
+			output += data.toString();
+		},
+		stderr: (data) => {
+			console.error(data);
+		}
+	};
+	await exec.exec(command, null, options);
+	return output;
 };
 
-const result = await exec.exec('ls', null, options);
-console.log(`${result}`);
+const main = async () => {
+	try {
+		const result = await execute('ls');
+		console.log(`${result}`);
+	} catch (error) {
+		core.setFailed(error.message);
+	}
+};
+main();
 
