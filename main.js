e3a48b3fd6ba06a95f3739c437e61af676800702
n0z0w0 <h02dvvd@gmail.com>
2022-10-16 16:14:52 +0900

    rewite main.js for js

--- main.js
+++ main.js
@@ -1,29 +1,19 @@
-const core = require('@actions/core')
-const github = require('@actions/github')
-const exec = require('@actions/exec')
-const writeGood = require('write-good')
+const core = require('@actions/core');
+const github = require('@actions/github');
+const exec = require('@actions/exec');
+const writeGood = require('write-good');
 
-const execute = async (command: string): Promise<string> => {
-	let output = ''
-	const options: ExecOptions = {}
-	options.listeners = {
-		stdout: (data: Buffer) => {
-			output += data.toString()
-		},
-		stderr: (data: Buffer) => {
-			console.error(data)
-		}
+let output = '';
+const options = {};
+options.listeners = {
+	stdout: (data: Buffer) => {
+		output += data.toString();
+	},
+	stderr: (data: Buffer) => {
+		console.error(data);
 	}
-	await exec.exec(command, null, options)
-	return output
-}
+};
 
-const main = aynsc () => {
-	try{
-		const result = await execute('ls')
-		console.log(`${result}`)
-	}catch (error){
-		core.setFailed(error.message)
-	}
-}
-main()
+const result = await exec.exec('ls', null, options);
+console.log(`${result}`);
+
