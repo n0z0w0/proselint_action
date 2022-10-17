660c7659ab884ef24919b4f441715d65118f0825
n0z0w0 <h02dvvd@gmail.com>
2022-10-17 14:55:50 +0900

    add error return

--- main.js
+++ main.js
@@ -26,12 +26,17 @@ const main = async () => {
 		await execute('ls');
 		const result = await execute("find -name \*.md");
 		const files = result.split('\n');
+		var isgood = true;
 		for (const file of files){
 			if (file === '') break;
 			console.log(file);
 			const text = fs.readFileSync(file, 'utf8');
 			const suggest = writeGood(text);
+			if (suggest.length >= 1) isgood = false;
 			console.log(suggest);
+		}
+		if (!isgood){
+			core.setFailed("Your writing is miserable.");
 		}
 	} catch (error) {
 		core.setFailed(error.message);
